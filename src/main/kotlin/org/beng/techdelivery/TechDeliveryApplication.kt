098907f607f6c9760c52850161bf4f6afd82d9b6
beng9re <kbh0581@gmail.com>
2024-08-17 18:49:18 +0900

    init: 프로젝트 생성

---
+++ src/main/kotlin/org/beng/techdelivery/TechDeliveryApplication.kt
@@ -0,0 +1,11 @@
+package org.beng.techdelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechDeliveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<TechDeliveryApplication>(*args)
+}
---
+++ src/test/kotlin/org/beng/techdelivery/TechDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.beng.techdelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechDeliveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
