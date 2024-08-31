2368bc46a798849661ec57f0bca0efa5cce00798
beng9re <kbh0581@gmail.com>
2024-08-31 22:52:25 +0900

    :package: Jpa 설정과 gradle 의존성 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "tech-delivery"
+
+pluginManagement {
+    val springBootVersion: String = extra["springBootVersion"] as String
+    val kotlinVersion: String = extra["kotlinVersion"] as String
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version "1.1.6"
+        id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    }
+}
